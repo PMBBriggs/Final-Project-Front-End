b359d280e7647d4168c2afd939ff922efa2c65af
Paige <Pbriggscreative@gmail.com>
2022-07-07 20:29:24 -0400

    searchPage saved

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,6 +3,14 @@ export default function Search(){
         <div>
             <label htmlFor="searchBox"></label>
             <input type='text' id="searchBox" placeholder="search game..."></input>
+            <p>Advanced Search</p>
+            <form>
+                <p>Genre</p>
+                <p>Platform</p>
+                <p>Mode</p>
+                {/* tags */}
+            </form>
+            {/* form for adv search thats hidden - clicking advanced search toggles */}
         </div>
     </div>)
 }
