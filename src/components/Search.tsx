ebf367b102821277f39c8af84a7bb0d4be75e518
Samantha Cummings <scummings1219@gmail.com>
2022-07-14 13:45:26 -0400

    added styles folder

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,31 +1,32 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { GetDetails } from "../Models/getDetails";
 import GetGames from "../Services/getGames";
 
-export default function Search(){
-    const [name, setName] = useState ('');
-    const [game, setGames] = useState <GetDetails[]>([]);
-    // const [search, setSearch] = useState ('');
+export default function Search() {
+  const [name, setName] = useState("");
+  const [game, setGames] = useState<GetDetails[]>([]);
+  // const [search, setSearch] = useState ('');
 
-    useEffect(() => {
-        GetGames(name).then(data => {
-          setGames(data);
-        });
-      }, [name]);
+  useEffect(() => {
+    GetGames(name).then((data) => {
+      setGames(data);
+    });
+  }, [name]);
 
-
-    return(<div className='SearchPage'>
-        <div>
-            <label htmlFor="searchBox"></label>
-            <input type='text' id="searchBox" placeholder="search game..."></input>
-            <p>Advanced Search</p>
-            <form>
-                <p>Genre</p>
-                <p>Platform</p>
-                <p>Mode</p>
-                {/* tags */}
-            </form>
-            {/* form for adv search thats hidden - clicking advanced search toggles */}
-        </div>
-    </div>)
+  return (
+    <div className="SearchPage">
+      <div>
+        <label htmlFor="searchBox"></label>
+        <input type="text" id="searchBox" placeholder="search game..."></input>
+        <p>Advanced Search</p>
+        <form>
+          <p>Genre</p>
+          <p>Platform</p>
+          <p>Mode</p>
+          {/* tags */}
+        </form>
+        {/* form for adv search thats hidden - clicking advanced search toggles */}
+      </div>
+    </div>
+  );
 }
