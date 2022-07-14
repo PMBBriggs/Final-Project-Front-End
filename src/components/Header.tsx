a53ddc3239d8b02041af6c25c31a1bb68d8fa8ef
dwmcnealy91 <87583311+dwmcnealy91@users.noreply.github.com>
2022-07-13 21:10:57 -0400

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,35 @@
+import "./Header.css";
 export default function Header() {
-    return(<div className='Header'>
-        <h1>Title Name Here.. </h1>
-        <nav>
-            <p>Home</p>
-            <p>Search</p>
-            <p>Wishlist</p>
-            <p>Profile</p>
-        </nav>
-     
-
-    </div>)
+  // /* Open */
+  // function openNav() {
+  //   document.getElementById("myNav").style.height = "100%";
+  // }
+  // /* Close */
+  // function closeNav() {
+  //   document.getElementById("myNav").style.height = "0%";
+  // }
+  // return (
+  //   <div className="Header">
+  //     {/* <!-- The overlay --> */}
+  //     <div id="myNav" className="overlay">
+  //       {/* <!-- Button to close the overlay navigation --> */}
+  //       <a
+  //         href="javascript:void(0)"
+  //         className="closebtn"
+  //         onClick={() => closeNav()}
+  //       >
+  //         &times;
+  //       </a>
+  //       {/* <!-- Overlay content --> */}
+  //       <div className="overlay-content">
+  //         <a href="#">About</a>
+  //         <a href="#">Services</a>
+  //         <a href="#">Clients</a>
+  //         <a href="#">Contact</a>
+  //       </div>
+  //     </div>
+  //     {/* <!-- Use any element to open/show the overlay navigation menu --> */}
+  //     <span onClick={() => openNav()}>open</span>
+  //   </div>
+  // );
 }
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,6 +3,6 @@ export default function HomePage(){
         <h2>Genre</h2>
         <h2>Platform</h2>
         <h2>Upcoming Titles</h2>
-        <h2>Recently Released</h2>
+        <h2>Recently Released</h2> 
     </div>)
 }
